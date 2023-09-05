3dc7b9d547d7ea709b62612046ae55d3f21dbe05
Guillermo <guichi.maldo@gmail.com>
2023-09-05 10:06:16 -0400

    Funcion que devuelve una palabra por posicion

--- src/funciones.js
+++ src/funciones.js
@@ -2,5 +2,21 @@ function devolverTexto(texto)
 {
     return texto;
 }
-
-export {devolverTexto};
+function devolver_una_palabra_por_posicion(texto,pos)
+{
+    let cont=1;
+    let palabra='';
+    for(let i=0;i<texto.length ;i++)
+    {
+        if(texto[i]==" " || texto[i]=="." || texto[i]=="," || texto[i]==";" || texto[i]=="-")
+        {
+            cont=cont+1;
+        }
+        else if(cont==pos)
+        {
+            palabra = palabra + texto[i];
+        }
+    }
+    return palabra;
+}
+export {devolverTexto,devolver_una_palabra_por_posicion};
--- src/funciones.test.js
+++ src/funciones.test.js
@@ -1,7 +1,10 @@
-import { devolverTexto } from "./funciones";
+import { devolverTexto, devolver_una_palabra_por_posicion} from "./funciones";
 
 describe("Funciones", () => {
   it("deberia devolver el texto ingresado", () => {
     expect(devolverTexto("El password de mi cuenta es PASSWORD. Es un password de poco cuidado")).toEqual("El password de mi cuenta es PASSWORD. Es un password de poco cuidado");
+  });
+  it("deberia devolver la palabra en la posicion x", () => {
+    expect(devolver_una_palabra_por_posicion("El password de mi cuenta es PASSWORD. Es un password de poco cuidado",5)).toEqual("cuenta");
   });
 });
